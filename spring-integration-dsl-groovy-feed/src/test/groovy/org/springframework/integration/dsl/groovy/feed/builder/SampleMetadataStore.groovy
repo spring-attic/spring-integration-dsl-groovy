92a0627084c0ed39db7a6b73eeb8dc6c45368469
Gary Russell <grussell@gopivotal.com>
2014-03-27 14:51:44 +0200

    Update SI to 3.0.1, Gradle to 1.11, Etc.
    Polishing `build.gradle`

--- spring-integration-dsl-groovy-feed/src/test/groovy/org/springframework/integration/dsl/groovy/feed/builder/SampleMetadataStore.groovy
+++ spring-integration-dsl-groovy-feed/src/test/groovy/org/springframework/integration/dsl/groovy/feed/builder/SampleMetadataStore.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2010 the original author or authors.
+ * Copyright 2002-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,8 @@
 
 package org.springframework.integration.dsl.groovy.feed.builder;
 
-import org.springframework.integration.store.MetadataStore
+import org.springframework.integration.metadata.MetadataStore
+import org.springframework.jmx.export.annotation.ManagedAttribute
 
 
 class SampleMetadataStore implements MetadataStore {
@@ -25,6 +26,12 @@ class SampleMetadataStore implements MetadataStore {
 	}
 
 	public String get(String key) {
+		return null;
+	}
+
+	@Override
+	@ManagedAttribute
+	String remove(String key) {
 		return null;
 	}
 }
