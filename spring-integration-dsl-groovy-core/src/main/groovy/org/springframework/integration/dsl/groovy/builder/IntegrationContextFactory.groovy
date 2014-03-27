23ec6b83d1b674727f97a940fc11ebcc7e976a01
Artem Bilan <abilan@gopivotal.com>
2014-03-28 00:23:33 +0200

    IDG-19: Upgrade to Groovy 2.2.2
    JIRA: https://jira.spring.io/browse/INTDSLGROOVY-19
    
    * Fix double `@Mixin` issue
    * Fix ambiguity of `IntegrationContext.createApplicationContext` methods, when `builder.parentContext == null`
    * Fix deprecated `dispathNodeCall` method usage

--- spring-integration-dsl-groovy-core/src/main/groovy/org/springframework/integration/dsl/groovy/builder/IntegrationContextFactory.groovy
+++ spring-integration-dsl-groovy-core/src/main/groovy/org/springframework/integration/dsl/groovy/builder/IntegrationContextFactory.groovy
@@ -1,36 +1,31 @@
 /*
- * Copyright 2002-2012 the original author or authors.
- * 
+ * Copyright 2002-2014 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
 package org.springframework.integration.dsl.groovy.builder
 
-import java.util.Map
-import groovy.util.FactoryBuilderSupport
-
-import org.apache.commons.logging.Log
-import org.apache.commons.logging.LogFactory
-
+import org.springframework.context.ApplicationContext
 /**
  * @author David Turanski
  *
  */
-class IntegrationContextFactory extends IntegrationComponentFactory { 
+class IntegrationContextFactory extends IntegrationComponentFactory {
 	protected Object doNewInstance(FactoryBuilderSupport builder, Object name, Object value, Map attributes){
 		return builder.integrationContext;
 	}
-	
+
 	@Override
 	void onNodeCompleted( FactoryBuilderSupport builder, Object parent, Object integrationContext ) {
 		if (builder.autoCreateApplicationContext) {
-			integrationContext.createApplicationContext(builder.parentContext)
+			integrationContext.createApplicationContext((ApplicationContext) builder.parentContext)
 		}
 	}
 }
